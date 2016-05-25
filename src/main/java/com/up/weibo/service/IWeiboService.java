8755d2f03810bcfade7e61f063033c475b6f4a4b
tyche <tyche@windows10.microdone.cn>
2016-05-25 20:29:37 +0800

    try adding find somebody's weibo

--- src/main/java/com/up/weibo/service/IWeiboService.java
+++ src/main/java/com/up/weibo/service/IWeiboService.java
@@ -9,4 +9,5 @@ public interface IWeiboService {
 	public List<WeiboEntity> getWeiboByUsername(String username);
 	public Long getWeiboAmount(String username);
 	public List<WeiboEntity> getAllWeibo();
+	public List<WeiboEntity> getWeiboByNickname(String nickname);
 }
